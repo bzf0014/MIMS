a1ca98897346d67d940a01a2b30d45fa84bef98c
Kaitlyn Randolph <knr0022@auburn.edu>
2019-04-29 01:00:30 -0500

    some changes

--- src/Patient.java
+++ src/Patient.java
@@ -1,3 +1,13 @@
 public class Patient {
     public String name;
+    public Chart chart;
+    public Appointment appointment;
+
+    public Appointment getAppointment() {
+        return appointment;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        this.appointment = appointment;
+    }
 }
